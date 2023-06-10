a3ecd4b14a1f1b6ba2afacb77077dea29014f97a
Arthur Ritzel <arthurritzel@users.noreply.github.com>
2023-06-10 19:42:02 -0300

    consulta

--- main.c
+++ main.c
@@ -10,7 +10,68 @@ int main(){
         char hora[6];
         float valor;
     }voo[20];
-    
+    voo[0].id = 1;
+    strcpy(voo[0].partida, "Sao Paulo");
+    strcpy(voo[0].destino, "Rio de Janeiro");
+    strcpy(voo[0].hora, "11:30");
+    voo[0].valor = 1000;
+
+    voo[1].id = 2;
+    strcpy(voo[1].partida, "Belo Horizonte");
+    strcpy(voo[1].destino, "Salvador");
+    strcpy(voo[1].hora, "14:45");
+    voo[1].valor = 1500;
+
+    voo[2].id = 3;
+    strcpy(voo[2].partida, "Recife");
+    strcpy(voo[2].destino, "Fortaleza");
+    strcpy(voo[2].hora, "09:20");
+    voo[2].valor = 1200;
+
+    voo[3].id = 4;
+    strcpy(voo[3].partida, "Porto Alegre");
+    strcpy(voo[3].destino, "Curitiba");
+    strcpy(voo[3].hora, "08:30");
+    voo[3].valor = 800;
+
+    voo[4].id = 5;
+    strcpy(voo[4].partida, "Brasília");
+    strcpy(voo[4].destino, "Manaus");
+    strcpy(voo[4].hora, "13:15");
+    voo[4].valor = 1800;
+
+    voo[5].id = 6;
+    strcpy(voo[5].partida, "Porto Seguro");
+    strcpy(voo[5].destino, "Natal");
+    strcpy(voo[5].hora, "10:40");
+    voo[5].valor = 1300;
+
+    voo[6].id = 7;
+    strcpy(voo[6].partida, "Florianópolis");
+    strcpy(voo[6].destino, "Foz do Iguaçu");
+    strcpy(voo[6].hora, "15:50");
+    voo[6].valor = 900;
+
+    voo[7].id = 8;
+    strcpy(voo[7].partida, "São Luís");
+    strcpy(voo[7].destino, "Belém");
+    strcpy(voo[7].hora, "12:20");
+    voo[7].valor = 1100;
+
+    voo[8].id = 9;
+    strcpy(voo[8].partida, "Goiânia");
+    strcpy(voo[8].destino, "Cuiabá");
+    strcpy(voo[8].hora, "14:00");
+    voo[8].valor = 950;
+
+    voo[9].id = 10;
+    strcpy(voo[9].partida, "João Pessoa");
+    strcpy(voo[9].destino, "Maceió");
+    strcpy(voo[9].hora, "17:30");
+    voo[9].valor = 1200;
+
+
+
     int escolha, very = 0;
     do{
         printf("----------------------------------------\n");
@@ -26,7 +87,15 @@ int main(){
             printf("----------------------------------------\n");
             printf("|           Consulta de voos           |\n");
             printf("----------------------------------------\n");
-
+            for (int i = 0; i < 10; i++){
+                printf("Id: %i\n", voo[i].id);
+                printf("Ponto de partida: %s\n", voo[i].partida);
+                printf("Destino: %s\n", voo[i].destino);
+                printf("Horario: %s\n", voo[i].hora);
+                printf("Valor: %.2f\n", voo[i].valor);
+                printf("-----------------------------\n\n");
+            }
+            
             break;
         case 2:
             /* Cadastro */
