b49de6cab503337f855f382353a4d8f706fa4f95
Arthur Ritzel <arthurritzel@users.noreply.github.com>
2023-06-20 00:33:51 -0300

    resenha excluir

--- excluir.h
+++ excluir.h
@@ -1,19 +1,27 @@
 int excluir(){
-    int idb = 0, flag = 0, opc = 0;
+    int idb = 0, flag = 0, opc = 0, esc = 0;
     do{
         flag = 0;
         printf("Digite o ID do voo que deseja excluir: ");
         scanf("%i", &idb);
         for (int i = 0; i < 20; i++) {
             if (idb == voo[i].id) {
-                voo[i].id = -1;
-                strcpy(voo[i].partida, " ");
-                strcpy(voo[i].destino, " ");
-                strcpy(voo[i].hora, " ");
-                voo[i].valor = 0;
-                printf("Voo excluído com sucesso!\n");
                 flag = 1;
-                break;
+                printf("\nPartida: %s\n", voo[i].partida);
+                printf("Destino: %s\n", voo[i].destino);
+                printf("Horario: %s\n", voo[i].hora);
+                printf("Valor: %.2f\n\n", voo[i].valor);
+                printf("Tem certeza que deseja excluir esse voo? (Essa acao nao sera reversivel)\n[1]Sim\n[2]Nao\n->");
+                scanf("%i", &esc);
+                    if(esc == 1){
+                    voo[i].id = -1;
+                    strcpy(voo[i].partida, " ");
+                    strcpy(voo[i].destino, " ");
+                    strcpy(voo[i].hora, " ");
+                    voo[i].valor = 0;
+                    printf("Voo excluído com sucesso!\n");
+                    break;
+                }
             }
         }
         if (flag != 1) {
